ae7a5ae5dcf0fec4e7f8266f424e2a03d06ade2e
Jessica Reginales <jessica.reginales@gmail.com>
2025-02-04 11:31:48 -0500

    simplifiedvariablesLab2.py

--- Lab2.py
+++ Lab2.py
@@ -1,11 +1,11 @@
 # Function 1: Calculate the height of the ball after time t
 # This function should take the initial height h0 and time t as inputs, and return the height at time t.
 def calculate_height(h0, t):
-    initial_height = float(input("Enter initial height: "))
+    h0= float(input("Enter initial height: "))
     gravity =9.8
-    time= float(input("Enter time: "))
-    height= (initial_height)-(((gravity)*(time**2))/2)
-    print(f"Height of the ball at time {time} second = {height} meters")
+    t= float(input("Enter time: "))
+    height= (h0)-(((gravity)*(t**2))/2)
+    print("height")
 
 # Function 2: Calculate the distance traveled by the car
 # This function should take the time t as input and return the distance traveled by the car.
@@ -13,4 +13,4 @@ def calculate_car_distance(t):
     speed =20
     time= float(input("Enter time for car (in seconds): "))
     distance= speed*time
-    print(f"The car will travel {distance} meters in {time} seconds")
+    print("distance")
